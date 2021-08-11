0acf6bdd4e8917aeb27e9e5566bd652fc4944ec8
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-08-11 13:20:56 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace AplicacionesArduino2021_II_G3
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            SeleccionarPuerto();
+        }
+
+        private void SeleccionarPuerto()
+        {
+            string[] V = SerialPort.GetPortNames();
+            Array.Sort(V);
+            tsComboPuertos.Items.AddRange(V);
+            
+        }
+
+        private void temperaturaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          FormTemperatura formTemperatura = new FormTemperatura();
+          formTemperatura.MdiParent = this;
+          formTemperatura.Show();
+
+        }
+
+       
+    }
+}
---
+++ FormTemperatura.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AplicacionesArduino2021_II_G3
+{
+    public partial class FormTemperatura : Form
+    {
+        StreamWriter sw;
+
+        public FormTemperatura()
+        {
+            InitializeComponent();
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Random temperaturaAleatoria = new Random();
+                sw = new StreamWriter("tiempoTemperatura.txt");
+
+                int renglones = 100;
+                sw.WriteLine("tiempo[s]  Temperatura [C]");
+
+                for (int i = 0; i < renglones; i++)
+                {
+                    string tiempo = i.ToString();
+                    string temperatura = temperaturaAleatoria.Next(1000).ToString();
+                    dgvDatos.Rows.Add(tiempo, temperatura);
+
+                }
+
+
+                for (int i = 0; i < renglones - 1; i++)
+                {
+                    string tiempo = dgvDatos.Rows[i].Cells[0].Value.ToString();
+                    string temperatura = dgvDatos.Rows[i].Cells[1].Value.ToString();
+
+                    sw.WriteLine(tiempo + "\t \t " + temperatura);
+                }
+            }
+
+            catch (IOException error)
+            {
+                MessageBox.Show("Error Archivo:" + error.Message);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error:  " + error.Message);
+            }
+
+            finally
+            {
+                sw.Close();
+            }
+
+
+        }
+    }
+}
